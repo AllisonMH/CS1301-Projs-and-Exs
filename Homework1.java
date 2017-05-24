76856de172edcc7d0a13999eb05a3247067e233d
AllisonMH <allyxh@gmail.com>
2017-05-24 01:18:43 -0400

    Homework 1 Desc
    This file is an introductory java assignment used to demonstrate variable use and output.

---
+++ Homework1.java
@@ -0,0 +1,10 @@
+// This file is an introduction to java assignment 
+public class Homework1 {
+	 public static void main(String[] args)  {
+			int age = 18;
+			String value = "Allison";
+			double annualPay = 100000;
+System.out.println ("My name is" + " " + value + " " + "my age is" + " " + age + " " +"and I hope to earn" + " " + annualPay);
+	
+}
+}
