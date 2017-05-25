51cbe0b59e09878c8bb3e0d86af98f13ad419de3
AllisonMH <allyxh@gmail.com>
2017-05-25 09:54:03 -0400

    Homework2 Desc
    This update utilizes static methods to update the gradeproject/homework2 file. More improvements will be made.

--- Homework2.java
+++ Homework2.java
@@ -25,3 +25,52 @@ public class GradeProject{
 	}
 	}
 }
+
+//An update to the Grade Project File 
+//if downloading and running, you must comment out one of these classes for the program to be sucessful or you will recieve an error
+
+
+
+public class GradeProject2{
+	static Scanner keyboard = new Scanner(System.in);
+	public static void main(String[]args)
+	{
+		System.out.print("Please enter five numeric test scores rounded to the nearest whole number:\n");
+		int score1 = keyboard.nextInt();
+		int score2 = keyboard.nextInt();
+		int score3 = keyboard.nextInt();
+		int score4 = keyboard.nextInt();
+		int score5 = keyboard.nextInt();
+		double Average = calcAverage(score1, score2, score3, score4, score5); 
+		System.out.println("Your average score is: \n" + Average);
+		System.out.println("Please enter your numeric average as calculated above:");
+		int testScore = keyboard.nextInt();
+		determineGrade(testScore);
+	}
+	public static double calcAverage(int score1, int score2, int score3, int score4, int score5)
+	{
+		return ((score1 + score2 + score3 + score4 + score5)/5);
+	}
+	public static void determineGrade(int testScore)
+	{
+		
+		if (testScore > 90)
+		{	
+			System.out.print('A');
+		}
+
+		else if(testScore >= 80){
+			System.out.print('B');
+		}
+		else if (testScore >= 70){
+			System.out.print('C'); 
+		}
+		else if (testScore >= 60){
+			System.out.print ('D');
+		}
+		else {
+			System.out.print ('F');
+		}
+	}
+
+}
